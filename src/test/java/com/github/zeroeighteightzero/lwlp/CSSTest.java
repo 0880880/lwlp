d14bf1146cf20a7c4a400f7ee3901ac3a546011a
0880 <98263509+0880880@users.noreply.github.com>
2024-12-16 18:33:02 +0330

    CSS tests

--- src/test/java/com/github/zeroeighteightzero/lwlp/CSSTest.java
+++ src/test/java/com/github/zeroeighteightzero/lwlp/CSSTest.java
@@ -1,41 +1,85 @@
 package com.github.zeroeighteightzero.lwlp;
 
+import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-import java.time.Instant;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class CSSTest {
 
-    public static void main(String[] args) {
+    private static CaseMatch whitespaceCaseFLR(CaseMatch caseMatch) {
+        return new ORMatch(
+                new ANDMatch("_",
+                        new TokenMatch("_", "WHITESPACE"),
+                        caseMatch,
+                        new TokenMatch("_", "WHITESPACE")
+                ),
+                new ANDMatch("_",
+                        new TokenMatch("_", "WHITESPACE"),
+                        caseMatch
+                ),
+                new ANDMatch("_",
+                        caseMatch,
+                        new TokenMatch("_", "WHITESPACE")
+                ),
+                caseMatch
+        );
+    }
+
+    private static CaseMatch whitespaceTokenFLR(String name, String type) {
+        return whitespaceCaseFLR(new TokenMatch(name, type));
+    }
+
+    public static String readResourceFile(String fileName) {
+        InputStream inputStream = CSSTest.class.getClassLoader().getResourceAsStream(fileName);
+        if (inputStream == null) throw new IllegalArgumentException("File not found: " + fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        } catch (Exception e) {
+            throw new RuntimeException("Error reading file: " + fileName, e);
+        }
+    }
+
+    public static void test(String fileName) {
         Lexer lexer = new Lexer(
-                new TokenPattern[] {
+                new TokenPattern[]{
+                        new TokenPattern("\\/\\*[\\s\\S]*?\\*\\/", "COMMENT").ignore(),
+                        new TokenPattern("\\s+", "WHITESPACE"),
                         new TokenPattern("\\{", "LBRACE"),
                         new TokenPattern("\\}", "RBRACE"),
-                        new TokenPattern("[\\w-]+", "IDENTIFIER"),
-                        new TokenPattern("\\d+(\\.\\d+)?[a-zA-Z]*", "NUMBER"),
+                        new TokenPattern("\\[", "LBRACKET"),
+                        new TokenPattern("\\]", "RBRACKET"),
+                        new TokenPattern("(?:\"([^\"\\\\]*(\\\\.[^\"\\\\]*)*)\"|'([^'\\\\]*(\\\\.[^'\\\\]*)*)')", "STRING"),
+                        new TokenPattern("[^\\W\\d][\\w-]*", "IDENTIFIER"),
+                        new TokenPattern("\\d+(\\.\\d+)?", "NUMBER"),
                         new TokenPattern("#[\\w-]+", "HASH"),
                         new TokenPattern("\\.[\\w-]+", "CLASS"),
                         new TokenPattern("\\,", "COMMA"),
                         new TokenPattern("\\:", "COLON"),
                         new TokenPattern("\\;", "SEMICOLON"),
-                        new TokenPattern("\\[[-_a-zA-Z0-9]+(?:([~|^$*]?=)[\"'][^\"']*[\"'])?\\]", "ATTR"),
-                        new TokenPattern("\\:[_a-zA-Z-]+", "PSEUDOCLASS"),
-                        new TokenPattern("\\:\\:[_a-zA-Z-]+", "PSEUDOELEM"),
+                        new TokenPattern("\\=", "EQUALS"),
                         new TokenPattern("[>~+]", "COMBINATOR"),
                 }
         );
 
-        Instant start = Instant.now();
-        List<Token> tokens = lexer.lex("body { background-color: lightblue; } h1 { color: darkblue; font-size: 24px; } p { color: black; font-family: Arial, sans-serif; }");
-        System.out.println("Lexer: Took " + Duration.between(start, Instant.now()).toMillis() + "ms");
-        System.out.println(tokens);
+        List<Token> tokens = lexer.lex(readResourceFile(fileName));
 
         Parser parser = new Parser(
                 new Definition("val",
                         new ORMatch(
-                                new TokenMatch("value", "IDENTIFIER"),
-                                new TokenMatch("value", "NUMBER")
+                                new TokenMatch("value_id", "IDENTIFIER"),
+                                new ANDMatch("value_unit",
+                                        new TokenMatch("value", "NUMBER"),
+                                        new TokenMatch("unit", "IDENTIFIER")
+                                ),
+                                new TokenMatch("value_num", "NUMBER"),
+                                new TokenMatch("value_str", "STRING")
                         ),
                         false
                 ),
@@ -45,12 +89,26 @@ public class CSSTest {
                                         new TokenMatch("tag", "IDENTIFIER"),
                                         new TokenMatch("id", "HASH"),
                                         new TokenMatch("class", "CLASS"),
-                                        new TokenMatch("universal", "UNIVERSAL"),
-                                        new TokenMatch("root", "ROOT")
-                                ),
-                                MultiMatch.min("attributes",
-                                        new TokenMatch("attribute", "ATTR"),
-                                        0
+                                        new ANDMatch("pseudo_class",
+                                                new TokenMatch("_", "COLON"),
+                                                new TokenMatch("class", "IDENTIFIER")
+                                        ),
+                                        new ANDMatch("attribute",
+                                                new ORMatch(
+                                                        new ANDMatch("_",
+                                                                new TokenMatch("_", "LBRACKET"),
+                                                                new TokenMatch("key", "IDENTIFIER"),
+                                                                new TokenMatch("equals", "EQUALS"),
+                                                                new DefMatch("value", "val"),
+                                                                new TokenMatch("_", "RBRACKET")
+                                                        ),
+                                                        new ANDMatch("_",
+                                                                new TokenMatch("_", "LBRACKET"),
+                                                                new TokenMatch("key", "IDENTIFIER"),
+                                                                new TokenMatch("_", "RBRACKET")
+                                                        )
+                                                )
+                                        )
                                 )
                         ),
                         false
@@ -59,15 +117,13 @@ public class CSSTest {
                         new OrderedMinMatch("compound_selector", 1,
                                 new DefMatch("simple_selector", "simple_selector"),
                                 MultiMatch.min("additional_selectors",
-                                        new ORMatch(
-                                                new DefMatch("simple_selector", "simple_selector"),
-                                                new TokenMatch("pseudo_class", "PSEUDOCLASS")
-                                        ),
+                                        new DefMatch("simple_selector", "simple_selector"),
                                         0
                                 ),
-                                MultiMatch.min("pseudo_element",
-                                        new TokenMatch("pseudo_element", "PSEUDOELEMENT"),
-                                        0
+                                new ANDMatch("pseudo_element",
+                                        new TokenMatch("_", "COLON"),
+                                        new TokenMatch("_", "COLON"),
+                                        new TokenMatch("element", "IDENTIFIER")
                                 )
                         ),
                         false
@@ -76,8 +132,14 @@ public class CSSTest {
                         new OrderedMinMatch("selector", 1,
                                 new DefMatch("compound_selector", "compound_selector"),
                                 MultiMatch.min("combinator_and_compound_selector",
-                                        new ANDMatch("combinator_with_selector",
-                                                new TokenMatch("combinator", "COMBINATOR"),
+                                        new ORMatch(
+                                                new ANDMatch("combinator_with_selector",
+                                                        new ORMatch(
+                                                                new TokenMatch("combinator", "COMBINATOR"),
+                                                                new TokenMatch("_", "WHITESPACE")
+                                                        ),
+                                                        new DefMatch("compound_selector", "compound_selector")
+                                                ),
                                                 new DefMatch("compound_selector", "compound_selector")
                                         ),
                                         0
@@ -87,30 +149,38 @@ public class CSSTest {
                 ),
                 new Definition("property",
                         new ANDMatch("property",
-                                new TokenMatch("property_name", "IDENTIFIER"),
-                                new TokenMatch("colon", "COLON"),
+                                whitespaceTokenFLR("property_name", "IDENTIFIER"),
+                                whitespaceTokenFLR("colon", "COLON"),
                                 new OrderedMinMatch("values", 1,
                                         new DefMatch("value", "val"),
                                         MultiMatch.min("_",
                                                 new ANDMatch("",
-                                                        new TokenMatch("_", "COMMA"),
+                                                        new ORMatch(
+                                                                new ANDMatch("_",
+                                                                        new TokenMatch("_", "COMMA"),
+                                                                        new TokenMatch("_", "WHITESPACE")
+                                                                ),
+                                                                new TokenMatch("_", "WHITESPACE")
+                                                        ),
                                                         new DefMatch("value", "val")
                                                 ),
                                                 0)
-                                        ),
-                                new TokenMatch("semicolon", "SEMICOLON")
+                                ),
+                                whitespaceTokenFLR("semicolon", "SEMICOLON")
                         ),
                         false
                 ),
                 new Definition("rule",
-                        new OrderedMinMatch("rule", 1,
-                                new DefMatch("selector", "selector"),
-                                new TokenMatch("lbrace", "LBRACE"),
-                                MultiMatch.min("properties",
-                                        new DefMatch("property", "property"),
-                                        0
-                                ),
-                                new TokenMatch("rbrace", "RBRACE")
+                        whitespaceCaseFLR(
+                                new OrderedMinMatch("rule", 1,
+                                        new DefMatch("selector", "selector"),
+                                        whitespaceTokenFLR("lbrace", "LBRACE"),
+                                        MultiMatch.min("properties",
+                                                new DefMatch("property", "property"),
+                                                0
+                                        ),
+                                        whitespaceTokenFLR("rbrace", "RBRACE")
+                                )
                         ),
                         true
                 )
@@ -118,6 +188,27 @@ public class CSSTest {
 
         parser.parse(tokens);
 
+    }
+
+    @Test
+    public void cssTest0() {
+        assertDoesNotThrow(() -> {
+            test("css_test0.css");
+        });
+    }
+
+    @Test
+    public void cssTest1() {
+        assertDoesNotThrow(() -> {
+            test("css_test1.css");
+        });
+    }
+
+    @Test
+    public void cssTest2() {
+        assertDoesNotThrow(() -> {
+            test("css_test2.css");
+        });
     }
 
 }
